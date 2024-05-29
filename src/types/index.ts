ae4aafcf61bcf7289651d741f20dddfff4f4a979
Anna Karelina <Karamelinaa94@yandex.ru>
2024-05-29 18:02:25 +0300

    документация

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface IProductItem {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number;
+}
+
+
+export interface IProductPreview {
+    products: IProductItem[];
+    preview: string | null;
+    getProduct(productId: string) :IProductItem;
+    setPreview(productId: string | null): void;  
+}
+
+export interface IProductInBasket {
+    productIndex: number; 
+    productInBasket: IProductItem[]; 
+    totalPrice: number;
+    getProductInBasket() :IProductItem[];
+    setProductInBasket(product: IProductItem[]): void;  
+    deleteProduct(product: IProductItem): void;
+}
+
+export interface IFormInfoOrder {
+    payment:string;
+    address:string;
+}
+
+export interface IFormInfoOrderCustomer {
+    email:string;
+    phone:string;
+}
+
+export interface IOrderSuccess {
+	id: string;
+	total: number;
+}
